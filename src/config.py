7078e4ad733399c47b4f550d65e1db04d5f77d6c
Valery Utukin <valeryvumvum@gmail.com>
2026-09-13 14:27:21 +0500

    Added Hotels model

--- src/config.py
+++ src/config.py
@@ -5,10 +5,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
-    DB_NAME: str
+    DB_HOST: str
     DB_PORT: int
     DB_USER: str
-    DB_PASSWORD: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DB_URL(self):
+        """
+        Формируем DSN для подключения к БД
+        :return:
+        """
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     model_config = SettingsConfigDict(
         # pathlib позволяет формировать путь
